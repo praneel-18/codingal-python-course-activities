33abf4c7492a427e9e926ffd8abf8ab6cb3ee3f2
Praneel <praneelsuyal@gmail.com>
2026-05-14 16:17:04 +0530

    finish lesson 4 activity 1

---
+++ lesson 4 activity 1/main.py
@@ -0,0 +1,20 @@
+# 1) Store the values for each tree in separate variables: tree1, tree2, tree3, tree4, tree5
+tree1 = 98
+tree2 = 94
+tree3 = 50
+tree4 = 11
+tree5 = 18
+
+# 2) Add all 5 tree values and store the result in `sum`
+ 
+sum=tree1 + tree2 + tree3 + tree4 + tree5 
+
+# 3) Print the total (`sum`) of all 5 trees
+print("sum of all trees", sum)
+
+# 4) Divide `sum` by 5 to find the average and store it in `average`
+average = sum/5
+
+
+# 5) Print the average (`average`) of all 5 trees
+print("average of trees", average)
