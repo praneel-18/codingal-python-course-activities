50a9a4d1423443eec83b47107c0719ba21777ec5
Praneel <praneelsuyal@gmail.com>
2026-05-16 11:19:30 +0530

    completed activity 3 of lesson 5

---
+++ lesson5 activity 3/mai.py
@@ -0,0 +1,10 @@
+num=int(input("enter a number:"))
+if num>50:
+    print("number is grater than 50")
+    print("if block was run")
+
+else:
+    print("number is less than 50")
+    print("else block was run")
+
+print("im not in if block and not in else block")
